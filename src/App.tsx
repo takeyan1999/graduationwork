5a47cbf5893e491bf786995996df330371a9999e
Takeru Iwaki <take10.tennis@gmail.com>
2024-06-11 20:10:40 +0900

    firstcommit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./css/reset.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./css/style.css";
+import QuizHeader from "./Components/QuizHeader";
+
+const App = () => {
+    return <QuizHeader />;
+};
+
+export default App;
